1395a90a177ff3ce44c86c39c8c508cffc020d03
sachin-niyana <sachinjakhar100@gmail.com>
2024-07-01 14:39:46 +0530

    merge code

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Solution from "@/components/solutions/Solution";
 import NavBar from "../components/common/NavBar";
 
 export default function Home() {
   return (
  <>
- <NavBar/>
+      <NavBar />
+      <Solution/>
  </>
   );
 }
--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -1,19 +1,36 @@
+import { Multi, Star, UtCoin } from "./Icon";
+
 export const navData = [
-    {
-      name: "Home",
-      to: "#aboutus",
-    },
-    {
-      name: "SBC",
-      to: "#ourservices",
-    },
- 
-    {
-      name: "Packs",
-      to: "#testimonials",
-    },
-    {
-      name: "Subscription",
-      to: "#contactus",
-    },
-  ];
+  {
+    name: "Home",
+    to: "#aboutus",
+  },
+  {
+    name: "SBC",
+    to: "#ourservices",
+  },
+
+  {
+    name: "Packs",
+    to: "#testimonials",
+  },
+  {
+    name: "Subscription",
+    to: "#contactus",
+  },
+];
+// ================================= SOLUTION ============================================//
+export const rateingList = [
+  {
+    icon: <Star />,
+    number: "93.08",
+  },
+  {
+    icon: <Multi />,
+    number: "4-3-3",
+  },
+  {
+    icon: <UtCoin />,
+    number: "13,000 Cost",
+  },
+];
