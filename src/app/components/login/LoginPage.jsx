dc1b9a8045eedc400307d589eaa7ef912c769dc4
naman-398 <you@example.com>
2024-07-01 12:12:07 +0530

    code check

--- src/app/components/login/LoginPage.jsx
+++ src/app/components/login/LoginPage.jsx
@@ -1,11 +1,22 @@
+"use client"
 import React from 'react'
-
+import bg from "../../../../public/assets/images/webp/loginBg.webp"
+import Image from 'next/image'
 const LoginPage = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div className='min-h-screen bg-vampire-black '>
+            <div className='max-w-[808px] w-full h-screen bg-cover bg-no-repeat bg-center ps-20' style={{
+                backgroundImage: `url(${bg.src})`,
+            }}>
+                <Image
+                    width={194.56}
+                    height={48}
+                    className='pt-20'
+                    src='/assets/images/svg/pageLogo.svg' />
+                <h2 className='font-semibold text-4xxl leadimg-121 !tracking-[-4%]'>The most accurate SBC solution and pack odds</h2>
+            </div>
+        </div>
+    )
 }
 
 export default LoginPage
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import LoginPage from "./components/login/LoginPage";
+import '../app/globals.css';
 
 export default function Home() {
   return (
  <>
+<LoginPage/>
  </>
   );
 }
