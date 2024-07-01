421e1be1fdd71f7315f0dcbe40a88cc97acbb9b3
sanjuverma373 <sv0240866@gmail.com>
2024-07-01 11:08:51 +0530

    push

--- src/app/components/Challenges/Squad.jsx
+++ src/app/components/Challenges/Squad.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Squad = () => {
   return (
-    <div className='bg-[#0A0B0C]'>
+    <div className='bg-[#0A0B0C] pt-[108px]'>
       <div className=' container max-w-[1600px] mx-auto px-3'>
-        
+        <h2 className='text-light-white font-inter font-semibold text-2xl leading-[29px]'>Challenges</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do </p>
       </div>
     </div>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Squad from "./components/Challenges/Squad";
 
 export default function Home() {
   return (
  <>
+ <Squad/>
  </>
   );
 }
