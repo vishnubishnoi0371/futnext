461c984677d17a804c801ee7da300a3e0cc5a021
sanjuverma373 <sv0240866@gmail.com>
2024-07-01 10:56:55 +0530

    commit

--- src/app/components/Challenges/Squad.jsx
+++ src/app/components/Challenges/Squad.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Squad = () => {
   return (
     <div className='bg-[#0A0B0C]'>
-      
+      <div className=' container max-w-[1600px] mx-auto px-3'>
+        
+      </div>
     </div>
   )
 }
