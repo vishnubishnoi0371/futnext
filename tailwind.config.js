d0b3a08aba57920cd95c04e0d4c64ea5ce6976af
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-07-01 10:18:13 +0530

    add root

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,37 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        "sm-13": "13px",
+        "3xxl": "32px",
+        "4xxl": "40px",
+      },
+    
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+     "font-gradient": 'linear-gradient(90deg, #00CA55 20.69%, #C3FF5A 58.36%)',
+      },
+      colors: {
+        "light-gray": "#e4e4e4",
+        "light-white": "#f4f4f4",
+        "old-silver": "#838383",
+        malachite: "#00ca55",
+        pigment: "#18a353",
+        "sonic-silver": "#787878",
+        "bright-gray": "#efefef",
+        "blue": "#58b9ff",
+        "yellow-green": "#98dd1f",
+        "silver-sand": "#c3c3c3",
+        Purple: "#de8eff",
+        mustard: "#cab600",
+        "black-olive": "#3b3c3d",
+        gray: "#7f7f7f",
+        "moss-green": "#35644a",
+        "eerie-black": "#17191b",
+
+      },
+      boxShadow: {
+        cardShadow: "0 5px 5px 0 #DEDEDE",
+        xs: "0 0 11px rgba(33,33,33,.2)",
       },
     },
   },
