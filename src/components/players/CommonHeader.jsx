6cfd8ed7111bafaa2b79767b8a4d1df5d55b2bfc
Your Name <you@example.com>
2024-07-02 10:27:43 +0530

    commit updates in generated

--- src/components/players/CommonHeader.jsx
+++ src/components/players/CommonHeader.jsx
@@ -6,37 +6,41 @@ import Image from 'next/image'
 const CommonHeader = () => {
     return (
         <>
-            <div className='pt-16 bg-vampire-black pb-[106px]'>
-                <div className='max-w-[1056px] mx-auto max-[2000px]:px-4  relative z-[1]'>
-                    <div className=' flex flex-row flex-wrap justify-between'>
-                        <div className='w-8/12'>
-                            <p className=' font-medium text-base text-malachite'>85 Rated Squad</p>
-                            <h2 className='text-4xl font-semibold !tracking-[-0.25px] text-light-white leading-155'>Weekly 90+ TOTS Player Pick <span className='text-light-gray font-normal text-base'>(Avg Return 200,00K)</span></h2>
-                            <p className='text-base font-normal text-light-gray leading-200'>Earn 1 of 3 rare gold players rated 85 overall or higher</p>
-                            <div className='my-6 gap-2 flex flex-col max-w-[270px]'>
-                                <div className='flex items-center gap-2 group'>
-                                    <Nonrepeat />
-                                    <p className='text-light-white text-base group-hover:opacity-60 duration-300 font-normal'>Non-Repeatable</p>
+            <div className='pt-16 bg-vampire-black pb-12 sm:pb-[106px]'>
+                <div className='max-w-[1088px] mx-auto px-4  relative z-[1]'>
+                    <div className=' flex flex-col lg:flex-row flex-wrap lg:justify-between justify-center items-center'>
+                        <div className='lg:w-8/12 max-lg:w-full'>
+                            <div className=' max-lg:flex flex-col max-lg:justify-center max-lg:items-center'>
+                                <p className=' font-medium text-base text-malachite'>85 Rated Squad</p>
+                                <h2 className='text-4xl max-lg:text-center font-semibold !tracking-[-0.25px] text-light-white leading-155'>Weekly 90+ TOTS Player Pick <span className='text-light-gray font-normal text-base block lg:inline'>(Avg Return 200,00K)</span></h2>
+                                <p className='text-base font-normal max-lg:text-center text-light-gray leading-200'>Earn 1 of 3 rare gold players rated 85 overall or higher</p>
+                                <div className='my-6 gap-2 flex flex-col max-w-[270px]'>
+                                    <div className='flex items-center gap-2 group'>
+                                        <Nonrepeat />
+                                        <p className='text-light-white text-base group-hover:opacity-60 duration-300 font-normal'>Non-Repeatable</p>
+                                    </div>
+                                    <div className='flex items-center gap-2 group'>
+                                        <Timer />
+                                        <p className='text-light-white text-base group-hover:opacity-60 duration-300 font-normal'>6 Days Remaining</p>
+                                    </div>
+                                    <div className='flex items-center gap-2 group'>
+                                        <Cost />
+                                        <p className='text-light-white text-base group-hover:opacity-60 duration-300 font-normal'>13,000 Cost</p>
+                                    </div>
+                                    <div className='flex items-center gap-2 group'>
+                                        <Saturated />
+                                        <p className='text-light-white text-nowrap text-base group-hover:opacity-60 duration-300 font-normal'>14,708 Solutions Generated</p>
+                                    </div>
                                 </div>
-                                <div className='flex items-center gap-2 group'>
-                                    <Timer />
-                                    <p className='text-light-white text-base group-hover:opacity-60 duration-300 font-normal'>6 Days Remaining</p>
-                                </div>
-                                <div className='flex items-center gap-2 group'>
-                                    <Cost />
-                                    <p className='text-light-white text-base group-hover:opacity-60 duration-300 font-normal'>13,000 Cost</p>
-                                </div>
-                                <div className='flex items-center gap-2 group'>
-                                    <Saturated />
-                                    <p className='text-light-white text-nowrap text-base group-hover:opacity-60 duration-300 font-normal'>14,708 Solutions Generated</p>
-                                </div>
+                                <PrimaryButton className="text-sm font-normal text-light-white !tracking-[-0.24px]" text="Generate Unique Solutions" />
                             </div>
-                            <PrimaryButton className="text-sm font-normal text-light-white !tracking-[-0.24px]" text="Generate Unique Solutions" />
                         </div>
-                        <div className=' w-[4/12]'>
-                            <Image src="/assets/images/webp/trianglesImg.png" alt='triangles' width={597} height={475} className=' absolute top-[-38px] z-[-1] right-[-140px] max-w-[597px] w-full' />
-                            <Image src="/assets/images/webp/globalPlate.png" alt='global' width={320} height={356} />
-                            <div className='w-[228px] h-[228px] rounded-full blur-[100px] absolute top-[60px] z-[-1] right-[40px] bg-malachite'></div>
+                        <div className=' lg:w-[4/12] max-lg:w-full max-lg:relative max-lg:mt-6'>
+                            <div className=' max-lg:flex flex-col max-lg:justify-center max-lg:items-center'>
+                                <Image src="/assets/images/webp/trianglesImg.png" alt='triangles' width={597} height={475} className=' absolute mx-auto lg:top-[-38px] !max-w-[597px] max-sm:max-h-[400px] bottom-[-90px] z-[-1] lg:right-[-124px] max-lg:translate-x-[0%] max-lg:translate-y-[0%] max-sm:top-[4%]' />
+                                <Image src="/assets/images/webp/globalPlate.png" alt='global' width={320} height={356} />
+                                <div className='w-[228px] h-[228px] rounded-full blur-[100px] absolute top-[60px] z-[-1] max-lg:translate-x-0 lg:right-[40px] bg-malachite'></div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/players/Generated.jsx
+++ src/components/players/Generated.jsx
@@ -4,30 +4,49 @@ import Image from 'next/image'
 import CommonHeader from './CommonHeader'
 import { TabArrowLeft, TabArrowRight } from '../common/Icon'
 import NavBar from '../common/NavBar'
+import Link from 'next/link'
 
 const Generated = () => {
     return (
         <>
-            <div className=' bg-vampire-black'>
+            <div className=' bg-vampire-black overflow-hidden'>
                 <CommonHeader />
-                <div className='max-w-[1172px] max-md:max-w-[476px] min-[1536px]:max-w-[1344px] min-[1800px]:max-w-[1600px] mx-auto max-xl:px-4'>
-                    <div className=' grid min-[1800px]:grid-cols-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8'>
+                <div className='max-w-[1172px] min-[1536px]:max-w-[1344px] min-[1800px]:max-w-[1600px] mx-auto max-xl:px-4'>
+
+                    <div className=' flex flex-wrap gap-6 justify-between items-center'>
+                        <div className=' flex flex-col'>
+                            <p className='text-2xl font-semibold text-light-white py-1'>Generated Solutions <span className=' text-base font-normal text-light-gray'>(150)</span> </p>
+                            <p className='text-dim-gray text-sm leading-150 py-[2px] font-normal tracking-[-0.25px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do </p>
+                        </div>
+                        <div className="relative after:content-[url(/assets/images/svg/selectArrow.svg)] h-12 border border-solid border-gray w-[144px] after:absolute rounded-[36px] after:top-[25%] after:right-[10%] after:pointer-events-none">
+                            <select name="latest" id="recent" className="bg-transparent focus:outline-none flex items-center px-4 py-[15.5px] text-sm text-light-white leading-121 font-normal ">
+                                <option className=' bg-black' value="recent">Most Recent</option>
+                                <option className=' bg-black' value="week">Last Week</option>
+                                <option className=' bg-black' value="month">Last Month</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div className='max-w-[1172px] max-md:max-w-[576px] min-[1536px]:max-w-[1344px] min-[1800px]:max-w-[1600px] mx-auto max-xl:px-4'>
+                    <div className=' grid min-[1800px]:grid-cols-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 pt-6'>
                         {playersData.map((obj, index) => {
                             return (
-                                <div key={index} className=' flex w-full justify-center'>
-                                    <div className='bg-black-olive h-[72px] p-4 w-full rounded-3xl'>
-                                        <div className='flex justify-between items-center'>
-                                            <div className='flex flex-col'>
-                                                <p className=' font-medium text-base text-white'>{obj.title}</p>
-                                                <p className=' font-medium text-sm-13 leading-123 text-dark-white'>{obj.description}</p>
-                                            </div>
-                                            <div className=' flex items-center gap-2'>
-                                                <Image src="/assets/images/svg/coinsImg.svg" alt='coin' height={32} width={32} />
-                                                <p className=' font-medium text-base lh-[19.36px] text-light-white'>{obj.cost}</p>
+                                <Link href="/login">
+                                    <div key={index} className=' flex w-full justify-center'>
+                                        <div className='bg-black-olive h-[72px] p-4 w-full rounded-3xl'>
+                                            <div className='flex justify-between items-center'>
+                                                <div className='flex flex-col'>
+                                                    <p className=' font-medium text-base text-white'>{obj.title}</p>
+                                                    <p className=' font-medium text-sm-13 leading-123 text-dark-white'>{obj.description}</p>
+                                                </div>
+                                                <div className=' flex items-center gap-2'>
+                                                    <Image src="/assets/images/svg/coinsImg.svg" alt='coin' height={32} width={32} />
+                                                    <p className=' font-medium text-base text-light-white'>{obj.cost}</p>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                </Link>
                             )
                         })}
                     </div>
